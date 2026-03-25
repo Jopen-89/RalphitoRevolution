5e61f54260251033ee3e4afd9da0a739b4387136
jopen <solimonra@gmail.com>
2026-03-25 21:42:50 +0100

    chore: remove legacy root test launchers

--- test-research.ts
+++
@@ -1,3 +0,0 @@
-import { createResearchTools } from './src/gateway/tools/researchTools.js';
-const tools = createResearchTools();
-tools[0].execute({ query: 'AI agents' }).then(console.log).catch(console.error);
